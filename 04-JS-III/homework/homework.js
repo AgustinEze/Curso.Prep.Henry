899f29f4eda081df8146d3b80cf1aebb14601965
AgustinEze <agustineg@gmail.com>
2021-07-18 20:56:28 -0300

    hw4 terminada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -121,13 +121,23 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(!arguments.length)
+    return 0;
+  var producto=1;
+  for (let i=0;i<arguments.length;i++)
+    producto*=arguments[i];
+  return producto;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador=0;
+  for(let i=0;i<arreglo.length;i++)
+    if(arreglo[i]>18)
+        contador++;
+  return contador;
 }
 
 
@@ -136,7 +146,18 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  switch(numeroDeDia){
+    case 1:
+        return "Es fin de semana";
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        return "Es dia Laboral";
+    case 7:
+        return "Es fin de semana";
+  }
 } 
 
 
@@ -144,7 +165,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  while(n>9){
+  n=Math.trunc(n/10);
+  }
+  if(n>=9) return true;
+  return false;
 }
 
 
@@ -152,7 +177,10 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (let i=0;i<arreglo.length;i++)
+    if(arreglo[0]!==arreglo[i])
+        return false;
+  return true;
 } 
 
 
@@ -161,6 +189,29 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var arr=[];
+  var flagEnero=flagMarzo=flagNoviembre=0;
+  for(let i=0;i<array.length;i++){
+    switch(array[i]){
+        case "Enero":
+            arr.push(array[i]);
+            flagEnero=1;
+            break;
+        case "Marzo":
+            arr.push(array[i]);
+            flagMarzo=1;
+            break;
+        case "Noviembre":
+            arr.push(array[i]);
+            flagNoviembre=1;
+            break;
+        default:
+            continue;
+    }
+  }
+  if(flagMarzo && flagNoviembre && flagEnero)
+    return arr;
+  return "No se encontraron los meses pedidos"
 }
 
 
@@ -168,6 +219,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var arr=[];
+
+  for(let i=0;i<array.length;i++)
+    if(array[i]>100)
+        arr.push(array[i]);
+
+  return arr;
 }
 
 
@@ -179,6 +237,14 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var arr=[];
+
+  for (let i=0;i<10;i++){
+    if(i===(numero+2*(i+1)))
+        return "Se interrumpió la ejecución";
+    arr.push(numero+2*(i+1));
+  }
+  return arr;
 }
 
 
@@ -189,6 +255,14 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var arr=[];
+
+  for(let i=0;i<10;i++){
+    if(i===4)
+        continue;
+    else arr.push(numero+=2);
+  }
+  return arr;
 }
 
 
